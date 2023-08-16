140637af82b6913802f3b6c84fdc5e4aea7d1e18
kishan-tekvortex <kishan.bhatta@tekvortex.com>
2023-08-16 10:53:28 +0545

    strings

--- PYTHON/strings.py
+++ PYTHON/strings.py
@@ -18,14 +18,35 @@ print(mystring[::]) # we can also give two colon :: for all
 print(mystring[::1]) # if the step size is one then it print everything
 print(mystring[::2]) # if the step size is two then it print the alternat character
 
-x = mystring.upper() # to print all in uppercase
-print(x)
-
-y = x.lower() #  for lowercase
-print(y)
-
-x = y.capitalize() # For first letter to be capital
-print (x)
+# x = mystring.upper() # to print all in uppercase
+# print(x)
+#
+# y = x.lower() #  for lowercase
+# print(y)
+#
+# x = y.capitalize() # For first letter to be capital
+# print (x)
+#
+# mystring = 'Hello Kishan Bhatta'
+# x = mystring.split()
+# print(x)  # which will print [Hello, Kishan, Bhatta]
+#
+# x = mystring.split('K')
+# print(x)  #which will print ['Hello', 'ishan Bhatta']
+#
+# x = mystring.split('B')
+# print(x)
 
 # Basic Methods
 # Print Formatting
+# For single string
+x = "Insert another string here: {}".format("INSERT ME!")
+print(x)
+
+# For multiple set of strings
+y = "Item One: {} Item Two: {} Item Three: {}".format("dog", "cat", "mouse")
+print(y)
+
+# You can also define the variable inside of the Formate
+x = "Item One: {x} Item Two: {y}".format(x="dog", y="cat") # you can able to change the variable
+print(x)
