eb959af564a605fef10ea17ad424bd02e6454673
kishan-tekvortex <kishan.bhatta@tekvortex.com>
2023-08-16 12:50:19 +0545

    python dictionaries

---
+++ PYTHON/dictionaries.py
@@ -0,0 +1,13 @@
+# Dictionaries
+#Syntax
+my_stuff = {"key1":"value", 'key2': 'value2'}
+print(my_stuff['key2'])
+
+my_stuff1 = {"Key1":123, "Key2":'value2', 'key3':{'123':[1,2,'grabMe']}}
+print(my_stuff1['key3']['123'][2].upper())
+
+my_stuff2 = {'lunch':'pizza', 'bfast': 'eggs'}
+my_stuff2['lunch'] ='burger'
+my_stuff['dinner'] = 'pasta'
+print(my_stuff2['lunch'])
+print(my_stuff)
