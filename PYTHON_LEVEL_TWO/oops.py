347ec0e73de3d75237d1ee81df3b9690b297d5f7
Kishan Bhatta <kishan.bhatta@tekvortex.local@ASSET078.tekvortex.local>
2023-08-24 12:08:45 +0545

    oop concept in python

--- PYTHON_LEVEL_TWO/oops.py
+++ PYTHON_LEVEL_TWO/oops.py
@@ -1,9 +1,31 @@
 # Object Oriented Programming is a away to use Python to create our own objects.
-
-
+# class Sample():
+#     pass
+# x = Sample()
+# print(type(x))
+print(type(()))  #It prints <type 'tuple'>
+print(type([]))  #It prints <type 'list'>
+print(type(200)) # It will prints <type 'int'>
+print(type(20.0)) # It will prints <type 'float>
 class Sample():
     pass
-
 x = Sample()
+print(type(x))  # It will print <type 'instanece'>
 
-print(type(x))
+class Dog():
+
+    # CLASS OBJECT ATTRIBUTE
+    species = "mammal"
+
+    def __init__(self,breed, name):
+        self.breed = breed
+        self.name = name
+#mydog = Dog(breed = "Lab", name = "Julie")
+mydog = Dog("Lab", "julie")  # Alternative for above line of code but have to put in order
+#otherdog = Dog(breed = "Huskie") # it will give us the out put as Huskie
+print(type(mydog))  # it will show the output of the dog class
+print(mydog.breed)  # it will show the output as the breed is Lab
+print(mydog.name)
+print(mydog.species)
+#print(otherdog.breed)
+
