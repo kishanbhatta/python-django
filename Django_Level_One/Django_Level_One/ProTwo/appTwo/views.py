d891b7a58ee400daacb788b948460da4fc7ba402
Kishan Bhatta <kishan.bhatta@tekvortex.local@ASSET078.tekvortex.local>
2023-09-28 17:55:23 +0545

    Django Level two

---
+++ Django_Level_One/Django_Level_One/ProTwo/appTwo/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return HttpResponse("<em>MY Second Project</em>")
+
+def help(request):
+    helpdict = {'help_insert': 'HELP PAGE'}
+    return render(request,'appTwo/help.html',context=helpdict)
